c9f7dc98f477cd830c6ebeddfee7ebb2902edd26
algisju <69957906+algisju@users.noreply.github.com>
2023-02-12 04:45:50 -0800

    Add files via upload

--- get-the-positions.py
+++ get-the-positions.py
@@ -1,20 +1,24 @@
 ##retrieve live timing motogp
 #search index.html for table class="timing_table" data-trsid="xxx"
 
-import urllib.request, json
+import urllib.request, json, os, time 
 
 url = "https://www.motogp.com/en/json/live_timing/685"
 
+
 response = urllib.request.urlopen(url)
-
 data = json.loads(response.read().decode())
-print(data["lt"] ["head"] ["session_name"])
 
-for x in range(1, 28):
-	try:
-		print(str(x)+" - "+data["lt"] ["rider"] [str(x)] ["rider_name"]+" "+data["lt"] ["rider"] [str(x)] ["rider_surname"] )
-	except KeyError:
-		break
+while (data["lt"] ["head"] ["session_status_id"]) =="F":
+	os.system("cls")
+	print(data["lt"] ["head"] ["session_name"]+"\n")
+
+	for x in range(1, 28):
+		try:
+			print(str(x)+" - "+data["lt"] ["rider"] [str(x)] ["rider_name"]+" "+data["lt"] ["rider"] [str(x)] ["rider_surname"] )
+		except KeyError:
+			break
+	time.sleep(60)
 
 
 
